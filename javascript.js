af4b017eeb584d9e5f49e14d4be2b70eeb2844d3
Igor Silveira <igorzollimsilveira@gmail.com>
2021-04-20 15:10:15 -0300

    Seção quem sou e Habilidades

--- javascript.js
+++ javascript.js
@@ -5,8 +5,8 @@ $(window).scroll(function () {
         Array.from(document.querySelectorAll('#navbar ul li a')).forEach(el => el.setAttribute("style", "color: #fff;"))
 
     }else{
-        document.getElementById("navbar").setAttribute("style", "background-color: #fff;")
-        Array.from(document.querySelectorAll('#navbar ul li a')).forEach(el => el.setAttribute("style", "color: #282828;"))
+        document.getElementById("navbar").setAttribute("style", "background-color:#15314e;")
+        Array.from(document.querySelectorAll('#navbar ul li a')).forEach(el => el.setAttribute("style", "color: #fff;"))
     }
 });
 /*Efeito menu principal [END] */
